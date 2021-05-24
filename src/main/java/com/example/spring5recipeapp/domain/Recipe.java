4a73a708a9fc29350796f925dd96185aaf473241
BeneZ <bezraat@gmail.com>
2021-05-24 13:24:15 +0200

    @ManytoMany

--- src/main/java/com/example/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/example/spring5recipeapp/domain/Recipe.java
@@ -35,4 +35,10 @@ public class Recipe {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredientSet;
 
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
 }
