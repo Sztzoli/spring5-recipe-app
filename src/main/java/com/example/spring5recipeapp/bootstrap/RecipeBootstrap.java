968d3387a572f6f249eb13b96f6c19cbb0bf7e98
BeneZ <bezraat@gmail.com>
2021-05-30 12:00:32 +0200

    display recipe continue

--- src/main/java/com/example/spring5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/example/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -107,6 +107,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.setPrepTime(10);
         guacRecipe.setCookTime(0);
         guacRecipe.setDifficulty(Difficulty.EASY);
+        guacRecipe.setServing(4);
+        guacRecipe.setSource("Simple Recipes");
+        guacRecipe.setUrl("https://www.oldelpaso.co.uk");
         guacRecipe.setDirections("1 Cut avocado, remove flesh: Cut the avocados in half. Remove seed. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon" +
                 "\n" +
                 "2 Mash with a fork: Using a fork, roughly mash the avocado. (Don't overdo it! The guacamole should be a little chunky.)" +
